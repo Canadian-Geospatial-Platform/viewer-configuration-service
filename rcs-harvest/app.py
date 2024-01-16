e9838935aa6656f4a5be1a8646e17ffd911a7550
Bo Lu <bo.lu@canada.ca>
2024-01-16 14:46:38 -0500

    Adds feature to return multiple viewer configurations from a single call, returned as an json array

--- rcs-harvest/app.py
+++ rcs-harvest/app.py
@@ -27,10 +27,7 @@ def lambda_handler(event, context):
     id = None
     lang = None
     message = ""
-    rcs_response = ""
-    gcs_response = ""
-    metadata_response = ""
-    
+
     try:
         id = str(event["id"])
     except:
@@ -42,11 +39,6 @@ def lambda_handler(event, context):
         message += ", lang was not supplied or is invalid"
     
     try:
-        key = str(event["key"])
-    except:
-        key = False
-
-    try:
         if (str(event["metadata"]).upper() == 'TRUE'):
             metadata = True
         else:
@@ -57,6 +49,8 @@ def lambda_handler(event, context):
     if not id or not lang:
         response = {"message": message}
         return response
+        
+    id_list = id.split(',')
     
     required = [False, False, metadata]
     keys = ["gcs", "rcs", "metadata"]
@@ -66,9 +60,9 @@ def lambda_handler(event, context):
     configuration = [viewer_configuration_table, rcs_configuration_path, geocore_api_path]
     
     
-    iterable_pool_data = [(id ,lang, False, viewer_configuration_table, 'gcs'), 
-                          (id, lang, False, rcs_configuration_path, 'rcs'),
-                          (id, lang, metadata, geocore_api_path, 'metadata')]
+    iterable_pool_data = [(id_list ,lang, True, viewer_configuration_table, 'gcs'), 
+                          (id_list, lang, True, rcs_configuration_path, 'rcs'),
+                          (id_list, lang, metadata, geocore_api_path, 'metadata')]
     
     with Pool() as p:
         response = p.starmap(get_generic, iterable_pool_data)
@@ -96,71 +90,82 @@ def lambda_handler(event, context):
     return response
 
 
-def get_generic(id, lang, required, path, key):
+def get_generic(id_list, lang, required, path, key):
     message = ""
     response = ""
     if key == "rcs":
-        rcs_url_request = path + "/" + lang + "/" + id
-        headers = {'Accept': 'application/json'}
-        rcs_response = requests.get(rcs_url_request, headers=headers)
-    
-        if rcs_response.ok:
-            response = json.loads(rcs_response.text)
-            if not response:
-                message = '{"rcs": "RCS not found"}'
+        #Note: RCS already supports the ability to return multiple ids from a single request
+        response = {}
+        lang_list = ['en', 'fr']
+        for lang in lang_list:
+            id = ",".join(id_list)
+            rcs_url_request = path + "/" + lang + "/" + id
+            headers = {'Accept': 'application/json'}
+            rcs_response = requests.get(rcs_url_request, headers=headers)
+
+            if rcs_response.ok:
+                response[lang] = json.loads(rcs_response.text)
+                
+                if not response:
+                    message = '{"rcs": "RCS not found"}'
+                else:
+                    message = '{"rcs": "Success returning RCS"}'
             else:
-                message = '{"rcs": "Success returning RCS"}'
-        else:
-            message += '{"rcs": "Could not access RCS: ' + rcs_url_request + '"}'
+                message += '{"rcs": "Could not access RCS: ' + rcs_url_request + '"}'
     elif key == "gcs":
+        #Note: GCS is obtained from a dynamodb table
         gcs_list = []
         try:
-            gcs_response = read_configuration_by_id(id, path, 'ca-central-1', dynamodb=None)
-            if gcs_response == []:
+            for id in id_list:
+                gcs_response = read_configuration_by_id(id, path, 'ca-central-1', dynamodb=None)
+                if gcs_response == []:
+                    response = json.loads("[]")
+                    message = '{"gcs": "GCS not found"}'
+                else:
+                    try:
+                        json_data = json.loads(gcs_response[0]['plugins'])
+                        try:
+                            gcs_list.append(json_data[0]['RAMPS'])
+                        except KeyError:
+                            try:
+                                gcs_list.append(json_data[0])
+                            except KeyError:
+                                gcs_list.append(json_data)
+        
+                    except KeyError:
+                        response = json.loads("[]")
+                        message = '{"gcs": "GCS does not have valid plugins"}'
+            
+            if gcs_list != []:
+                response = gcs_list
+                message = '{"gcs": "Success returning GCS"}'
+            else:
                 response = json.loads("[]")
                 message = '{"gcs": "GCS not found"}'
-            else:
-                try:
-                    json_data = json.loads(gcs_response[0]['plugins'])
-                    try:
-                        gcs_list.append(json_data[0]['RAMPS'])
-                    except KeyError:
-                        try:
-                            gcs_list.append(json_data[0])
-                        except KeyError:
-                            gcs_list.append(json_data)
-    
-                    if gcs_list != []:
-                        response = gcs_list
-                        message = '{"gcs": "Success returning GCS"}'
-                    else:
-                        response = json.loads("[]")
-                        message = '{"gcs": "GCS not found"}'
-                except KeyError:
-                    response = json.loads("[]")
-                    message = '{"gcs": "GCS does not have valid plugins"}'
         except:
             response = json.loads("[]")
             message = '{"gcs": "Error returning GCS"}'
     elif key == "metadata":
         metadata_list = []
         if required == True:
-            metadata_url_request = path + "?lang=" + lang + "&id=" + id
-            headers = {'Accept': 'application/json'}
-            metadata_response = requests.get(metadata_url_request, headers=headers)
-        
-            if metadata_response.ok:
-                try:
-                    temp = json.loads(metadata_response.text)['body']['Items']
-                    metadata_list.append(temp)
-                    response = metadata_list
-                    message = '{"metadata": "Success returning metadata"}'
-                except (KeyError, TypeError) as e:
+            for id in id_list:
+                metadata_url_request = path + "?lang=" + lang + "&id=" + id
+                headers = {'Accept': 'application/json'}
+                metadata_response = requests.get(metadata_url_request, headers=headers)
+            
+                if metadata_response.ok:
+                    try:
+                        temp = json.loads(metadata_response.text)['body']['Items']
+                        metadata_list.append(temp)
+                    except (KeyError, TypeError) as e:
+                        response = json.loads("[]")
+                        message = '{"metadata": "Metadata not found"}'
+                else:
                     response = json.loads("[]")
-                    message = '{"metadata": "Metadata not found"}'
-            else:
-                response = json.loads("[]")
-                message = '{"metadata": "Could not access metadata: ' + metadata_url_request + '"}'
+                    message = '{"metadata": "Could not access metadata: ' + metadata_url_request + '"}'
+            
+            response = metadata_list
+            message = '{"metadata": "Success returning metadata"}'
         else:
             response = json.loads("[]")
             message = '{"metadata": "Metadata not requested"}'
@@ -186,89 +191,3 @@ def read_configuration_by_id(uuid, viewer_configuration_table, region, dynamodb=
 def nonesafe_loads(obj):
     if obj is not None:
         return json.loads(obj)
-        
-
-"""
-
-def get_gcs(id, lang, required, viewer_configuration_table):
-    message = ""
-    gcs_response = read_configuration_by_id(id, viewer_configuration_table, 'ca-central-1', dynamodb=None)
-
-    # Access the first element in the list
-    first_element = gcs_response[0]
-    # Convert the JSON string to a dictionary
-    json_string = first_element['plugins']
-    json_data = json.loads(json_string)
-    # Now we can access the nested structure
-    gcs_response = json_data[0]['RAMPS']
-
-    if gcs_response != None:
-        message += '{"gcs": "Success returning GCS"}'
-    else:
-        message += '{"gcs": "Could not access GCS: ' + id + '}"'
-    
-    return gcs_response, message
-
-def get_rcs(id, lang, required, rcs_configuration_path):
-    message = ""
-    rcs_url_request = rcs_configuration_path + "/" + lang + "/" + id
-    
-    #Add headers to accept JSON
-    headers = {'Accept': 'application/json'}
-    #Use requests to get the RCS
-    rcs_response = requests.get(rcs_url_request, headers=headers)
-
-    if rcs_response.ok:
-        rcs_response = json.loads(rcs_response.text)
-        message += '{"rcs": "Success returning RCS"}'
-    else:
-        message += '{"rcs": "Could not access RCS: ' + rcs_url_request + '"}'
-
-    return rcs_response, message
-    
-def get_metadata(id, lang, required, geocore_api_path):
-    message = ""
-    if required == True:
-        metadata_url_request = geocore_api_path + "?lang=" + lang + "&id=" + id
-        #Add headers to accept JSON
-        headers = {'Accept': 'application/json'}
-        #Use requests to get the geocore metadata
-        metadata_response = requests.get(metadata_url_request, headers=headers)
-        
-        if metadata_response.ok:
-            metadata_response = json.loads(metadata_response.text)['body']['Items']
-            message += '{"metadata": "Success returning metadata"}'
-        #else:
-            #message += '{"metadata": "Could not access metadata: ' + metadata_url_request + '"}'
-    else:
-        metadata_response = ""
-        message += '{"metadata": "metadata not requested"}'
-    
-    return metadata_response, message
-
-    #Fetch the GCS configs
-    try:
-        gcs_response, gcs_message = get_gcs(id, lang, required, viewer_configuration_table)
-        message = json.loads(gcs_message)
-    except:
-        message.update(json.loads('{"gcs": "gcs error"}'))
-        
-    
-    #Fetch the RCS configs
-    try:
-        rcs_response, rcs_message = get_rcs(id, lang, required, rcs_configuration_path)
-        rcs_message = json.loads(rcs_message)
-        message.update(rcs_message)
-    except:
-        message.update(json.loads('{"rcs": "rcs error"}'))
-
-    
-    #Fetch geocore metadata
-    try:
-        metadata_response, metadata_message = get_metadata(id, lang, required, geocore_api_path)
-        metadata_message = json.loads(metadata_message)
-        message.update(metadata_message)
-    except:
-        message.update(json.loads('{"metadata": "metadata error"}'))
-
-    """
